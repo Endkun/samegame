1cefaed3a3e0054e51b2cd3dbd983c57d687b19c
Ryusei Endo <riyu02180218@gmail.com>
2025-06-07 20:47:26 +0900

    検知して数えるやつをscriptに移行

--- script.js
+++ script.js
@@ -13,17 +13,12 @@ class BoredBlocks{
         this.blue = new Image();
         this.blue.src = 'img/blue.png';
         this.white = new Image();
-        this.white.src = 'img/white.png';
+        this.white.src = 'img/green.png';
         this.yellow = new Image();
         this.yellow.src = 'img/yellow.png';
         this.pink = new Image();
         this.pink.src = 'img/pink.png';
         this.tiles = [this.red,this.blue,this.white,this.yellow,this.pink]
-    }
-    //example
-    //[[0,0,1,1,2,3,2,0,3,0,1,1],
-    // [3,1,0,1,3,0,0,1,2,2,3,2]...]
-    update(ctx){
         if (this.board.length == 0){
             for (let i = 0; i < 10; i++) {
                 let line = [];
@@ -32,19 +27,23 @@ class BoredBlocks{
                 }
                 this.board.push(line);
             }
-            //console.log("board=",this.board);
-        }else{
-            for ( let i = 0;  i < 10;  i++ ) {
-                for ( let j = 0; j < 15; j++) {
-                    this.mapnum = parseInt(this.board[i][j]); 
-                    ctx.drawImage(this.tiles[this.mapnum], 0, 0, this.w, this.h, this.mx, this.my, this.w, this.h );
-                    this.mx += 50;
-                }
-                this.mx = 0;
-                this.my += 50;
-            }
-            this.my = 0;
         }
+    //example
+    //[[0,0,1,1,2,3,2,0,3,0,1,1],
+    // [3,1,0,1,3,0,0,1,2,2,3,2]...]
+    }
+    draw(ctx){
+        for ( let i = 0;  i < 10;  i++ ) {
+            for ( let j = 0; j < 15; j++) {
+                this.mapnum = parseInt(this.board[i][j]); 
+                ctx.drawImage(this.tiles[this.mapnum], 0, 0, this.w, this.h, this.mx, this.my, this.w, this.h );
+                this.mx += 50;
+            }
+                
+            this.mx = 0;
+            this.my += 50;
+        }
+        this.my = 0;
     }
 }
 class Click{
@@ -59,14 +58,14 @@ class Click{
             let rect = e.target.getBoundingClientRect();
             this.cx = e.clientX - rect.left;//cx = eventで読み取られたx-キャンバスの大きさの左端
             this.cy = e.clientY - rect.top;
-            this.dy = this.cx/50;
-            this.dx = this.cy/50;
+            this.dy = this.cy/50;
+            this.dx = this.cx/50;
             this.dy = parseInt(this.dy);//2.52や2.79とかになって配列が検知できずエラーが起きるためint化
             this.dx = parseInt(this.dx);
-            console.log(board[this.dy][this.dx])
+            //console.log(board[this.dy][this.dx])
             this.cp = board[this.dy][this.dx] //クリックされた色の記号
             let count = this.direction(board)
-            //console.log("count",count)
+            console.log("count",count)
             //[[0,0,1,1,2,3,2,0,3,0,1,1],
             // [3,1,0,1,3,0,0,1,2,2,3,2]...]
         });
@@ -90,7 +89,7 @@ class Click{
         ];
         while (stack.length > 0) {
             const [y, x] = stack.pop();//今の場所のスタックを取り除く
-            if (y < 0 || x < 0 || y >= board || x >= board[0].length || visited[y][x] === 1 || board[y][x] !== this.cp){//範囲から外れているか、今のdydxが3か、一度いたかでなかったら戻る
+            if (y < 0 || x < 0 || y >= board.length || x >= board[0].length || visited[y][x] === 1 || board[y][x] !== this.cp){//範囲から外れているか、今のdydxが3か、一度いたかでなかったら戻る
                 continue;//最初のwhileに戻る(breakだとwhileから抜けてしまうため使わない) 
             }
             visited[y][x] = 1;
@@ -99,7 +98,7 @@ class Click{
                 let newy = y + ny;
                 let newx = x + nx;
                 stack.push([newy, newx]);
-                //console.log(dirx,diry)
+                //console.log(dix,diry)
             }
         }
 
@@ -114,14 +113,14 @@ function main(){
     function loop(){
         ctx.fillStyle = "white"
         ctx.fillRect(0, 0, canvasx.width, canvasx.height); // 背景を白
-        // 2. 図形の描画
+        // 2. 図形の描画r
         ctx.fillStyle = 'rgb(10, 10, 10)';
         ctx.font = "50px serif";
         ctx.beginPath();
         //ctx.fillRect(20, 300, 50, 50)
         ctx.fill();
         //ctx.font = '55px Arial'; // フォント設定
-        B.update(ctx);
+        B.draw(ctx);
         //ctx.fillStyle = 'blue'; 
         //ctx.fillText('Hello World', 20, 100); // (20, 100)の位置に文字を表示
         requestAnimationFrame(loop);
